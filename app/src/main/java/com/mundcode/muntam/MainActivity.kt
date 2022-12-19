3111c5376e5aea6b37fa58ba2455572ed02dc607
sorea.no <sorae.no@thingsflow.com>
2022-12-19 09:08:16 +0900

    Feat: 진입점 수정

--- app/src/main/java/com/mundcode/muntam/MainActivity.kt
+++ app/src/main/java/com/mundcode/muntam/MainActivity.kt
@@ -4,40 +4,48 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.mundcode.muntam.ui.theme.MuntamTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MuntamTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MuntamApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
+fun MuntamApp() {
     MuntamTheme {
-        Greeting("Android")
+        val navController = rememberNavController()
+        val currentBackstack by navController.currentBackStackEntryAsState()
+        val currentDestination by remember {
+            derivedStateOf {
+                currentBackstack?.destination
+            }
+        }
+
+        Scaffold { innerPadding ->
+            MuntamNavHost(
+                navController = navController,
+                startDestination = Main,
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
+
     }
 }
