d5b334cce4b4dd6ff8d08f2e6c1eccaabf72a67b
nosorae <shthfo@gmail.com>
2022-12-21 03:27:23 +0900

    Feat: Dimen 추가

---
+++ app/src/main/java/com/mundcode/muntam/presentation/ui/theme/Dimen.kt
@@ -0,0 +1,12 @@
+package com.mundcode.muntam.presentation.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val DefaultSpace2 = 2.dp
+val DefaultSpace4 = 4.dp
+val DefaultSpace8 = 8.dp
+val DefaultSpace12 = 12.dp
+val DefaultSpace16 = 16.dp
+val DefaultSpace32 = 32.dp
+val DefaultSpace48 = 48.dp
+
