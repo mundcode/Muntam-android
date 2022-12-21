8729944052412c16dff9e3509814783d1559e2a4
nosorae <shthfo@gmail.com>
2022-12-21 09:35:44 +0900

    Fix: Toolbar 타이틀 얼라인 안맞는 문제 수정

--- app/src/main/java/com/mundcode/muntam/presentation/ui/component/Toolbar.kt
+++ app/src/main/java/com/mundcode/muntam/presentation/ui/component/Toolbar.kt
@@ -1,6 +1,5 @@
 package com.mundcode.muntam.presentation.ui.component
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -10,10 +9,9 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.mundcode.muntam.presentation.ui.theme.DefaultSpace16
-import com.mundcode.muntam.presentation.ui.theme.DefaultSpace4
 import com.mundcode.muntam.presentation.ui.theme.DefaultSpace8
 
 @Composable
@@ -31,14 +29,19 @@ fun MuntamToolbar(
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Row {
+        Row(
+            modifier = Modifier.fillMaxHeight(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             if (showBack) {
                 Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
+                MarginSpacer(dp = DefaultSpace8)
             }
 
-            MarginSpacer(dp = DefaultSpace4)
-
-            Text(text = title, style = MaterialTheme.typography.h6)
+            Text(
+                text = title,
+                style = MaterialTheme.typography.h6
+            )
         }
 
         Row(horizontalArrangement = Arrangement.spacedBy(DefaultSpace8)) {
@@ -47,6 +50,5 @@ fun MuntamToolbar(
             }
         }
     }
-
 }
 
