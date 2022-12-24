a27619e27d7165e24f4d7297c9dc92144c1d6046
nosorae <shthfo@gmail.com>
2022-12-25 04:53:50 +0900

    Feat: 메인 액티비티에서 알림 권한요청

--- app/src/main/java/com/mundcode/muntam/MainActivity.kt
+++ app/src/main/java/com/mundcode/muntam/MainActivity.kt
@@ -1,18 +1,56 @@
 package com.mundcode.muntam
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.mundcode.muntam.presentation.ui.theme.MuntamTheme
 
+
+
 class MainActivity : ComponentActivity() {
+    private val requestNotificationPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { ok ->
+            if (ok) {
+                // 알림권한 허용 o
+            } else {
+                // 알림권한 허용 x. 자유롭게 대응..
+            }
+        }
+
+
+    fun requestNotificationPermission() {
+        if (
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                    // 왜 알림을 허용해야하는지 알려주기 권장
+                } else {
+                    requestNotificationPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                }
+            } else {
+                // 안드로이드 12 이하는 알림에 런타임 퍼미션 없으니, 설정가서 켜보라고 권해볼 수 있겠다.
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
