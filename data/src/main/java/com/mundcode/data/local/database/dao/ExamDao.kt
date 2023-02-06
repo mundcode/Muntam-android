308857feeeaedd354a90f62459eea34e90b8faa8
nosorae <shthfo@gmail.com>
2023-02-07 00:10:46 +0900

    chore: 시험 데이터 클래스 프로퍼티, DAO 수정

--- data/src/main/java/com/mundcode/data/local/database/dao/ExamDao.kt
+++ data/src/main/java/com/mundcode/data/local/database/dao/ExamDao.kt
@@ -2,6 +2,8 @@ package com.mundcode.data.local.database.dao
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Update
 import com.mundcode.data.local.database.model.ExamEntity
 import kotlinx.coroutines.flow.Flow
 import kotlinx.datetime.Instant
@@ -17,8 +19,17 @@ abstract class ExamDao : BaseDao<ExamEntity> {
 
     @Query(
         value = """
-            UPDATE exams SET deleted_at=:deletedAt WHERE id IN (:ids)
+            UPDATE exams SET deleted_at=:deletedAt WHERE id = :id
         """
     )
-    abstract suspend fun deleteExams(ids: List<Int>, deletedAt: Instant)
+    abstract suspend fun deleteExam(id: Int, deletedAt: Instant)
+
+    @Update
+    abstract suspend fun updateExam(exam: ExamEntity)
+
+    @Transaction
+    open suspend fun deleteExamWithCasacade(id: Int, deletedAt: Instant) {
+        deleteExam(id, deletedAt)
+        // todo 시험삭제하면서 딸린 문제까지 지우는 @Transaction 작성하고 deleteExam 대체
+    }
 }
