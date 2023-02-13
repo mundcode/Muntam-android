170303776fe0ec1ef3fdd180b778113063c9a055
nosorae <shthfo@gmail.com>
2023-02-14 01:26:27 +0900

    feat: QuestionRepositoryImpl 추가 구현

--- data/src/main/java/com/mundcode/data/repository/QuestionRepositoryImpl.kt
+++ data/src/main/java/com/mundcode/data/repository/QuestionRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.mundcode.data.repository
 
 import com.mundcode.data.local.database.dao.QuestionDao
+import com.mundcode.data.local.database.model.asEntity
 import com.mundcode.domain.model.Question
 import com.mundcode.domain.repository.QuestionRepository
 import kotlinx.coroutines.flow.Flow
@@ -9,27 +10,19 @@ import javax.inject.Inject
 class QuestionRepositoryImpl @Inject constructor(
     private val questionDao: QuestionDao
 ) : QuestionRepository {
-    override suspend fun insertQuestion(question: Question) {
-        TODO("Not yet implemented")
-    }
+    override suspend fun insertQuestion(question: Question) =
+        questionDao.insert(question.asEntity())
 
-    override suspend fun insertQuestions(questions: List<Question>) {
-        TODO("Not yet implemented")
-    }
+    override suspend fun insertQuestions(questions: List<Question>) =
+        questionDao.insertAll(questions.map { it.asEntity() })
 
-    override fun getQuestions(examId: Int): Flow<List<Question>> {
-        TODO("Not yet implemented")
-    }
+    override fun getQuestions(examId: Int): Flow<List<Question>> =
+        questionDao.getQuestions(examId)
 
-    override fun getQuestionExamId(examId: Int, questionNumber: Int): Flow<Question> {
-        TODO("Not yet implemented")
-    }
+    override fun getQuestionExamId(examId: Int, questionNumber: Int): Flow<Question> =
+        questionDao.getQuestionExamId(examId, questionNumber)
 
-    override fun getQuestionById(id: Int): Flow<Question> {
-        TODO("Not yet implemented")
-    }
+    override fun getQuestionById(id: Int): Flow<Question> = questionDao.getQuestionById(id)
 
-    override suspend fun updateQuestion(question: Question) {
-        TODO("Not yet implemented")
-    }
+    override suspend fun updateQuestion(question: Question) = questionDao.updateQuestion(question)
 }
