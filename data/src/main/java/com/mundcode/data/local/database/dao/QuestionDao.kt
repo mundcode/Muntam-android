072afea07ef43f830403e1422065c033417e1dc8
nosorae <shthfo@gmail.com>
2023-02-14 01:25:04 +0900

    feat: QuestionDao 추가

--- data/src/main/java/com/mundcode/data/local/database/dao/QuestionDao.kt
+++ data/src/main/java/com/mundcode/data/local/database/dao/QuestionDao.kt
@@ -1,7 +1,33 @@
 package com.mundcode.data.local.database.dao
 
+import androidx.room.Query
+import androidx.room.Update
 import com.mundcode.data.local.database.model.QuestionEntity
+import com.mundcode.domain.model.Question
+import kotlinx.coroutines.flow.Flow
 
 abstract class QuestionDao : BaseDao<QuestionEntity> {
+    @Query(
+        value = """
+            SELECT * FROM questions WHERE deleted_at IS NULL AND exam_id = :examId
+        """
+    )
+    abstract fun getQuestions(examId: Int): Flow<List<Question>>
 
+    @Query(
+        value = """
+            SELECT * FROM questions WHERE deleted_at IS NULL AND exam_id = :examId AND question_number = :questionNumber
+        """
+    )
+    abstract fun getQuestionExamId(examId: Int, questionNumber: Int): Flow<Question>
+
+    @Query(
+        value = """
+            SELECT * FROM questions WHERE deleted_at IS NULL
+        """
+    )
+    abstract fun getQuestionById(id: Int): Flow<Question>
+
+    @Update
+    abstract suspend fun updateQuestion(question: Question)
 }
