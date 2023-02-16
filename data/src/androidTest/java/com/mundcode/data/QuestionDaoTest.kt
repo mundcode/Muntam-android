cdaa4131421dfd77ee243abc676c2821e0ea794e
nosorae <shthfo@gmail.com>
2023-02-16 20:48:51 +0900

    feat: 문제 읽고 쓰기 추가

--- data/src/androidTest/java/com/mundcode/data/QuestionDaoTest.kt
+++ data/src/androidTest/java/com/mundcode/data/QuestionDaoTest.kt
@@ -5,15 +5,26 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.mundcode.data.local.database.MuntamDatabase
+import com.mundcode.data.local.database.dao.ExamDao
 import com.mundcode.data.local.database.dao.QuestionDao
+import com.mundcode.data.local.database.dao.SubjectDao
+import com.mundcode.data.local.database.model.createExamEntity
+import com.mundcode.data.local.database.model.createSubjectEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.IOException
 
 @RunWith(AndroidJUnit4::class)
 class QuestionDaoTest {
     private lateinit var questionDao: QuestionDao
+    private lateinit var subjectDao: SubjectDao
+    private lateinit var examDao: ExamDao
     private lateinit var db: MuntamDatabase
 
     @Before
@@ -24,11 +35,30 @@ class QuestionDaoTest {
             MuntamDatabase::class.java
         ).build()
         questionDao = db.questionDao()
+        subjectDao = db.subjectDao()
+        examDao = db.examDao()
     }
 
     @After
     @Throws(IOException::class)
     fun closeDb() {
         db.close()
+    }
+
+    @Test
+    fun testWriteAndReadExam() = runBlocking(Dispatchers.IO) {
+        // subjectId 과 examId 모두 0 으로 하면 Foreign Key Constraint Failed  에러
+        val subjectId = 1
+        val examId = 2
+        val subjectEntity = createSubjectEntity(subjectId)
+        val examEntity = createExamEntity(id = examId, subjectId = subjectId)
+
+        subjectDao.insert(subjectEntity)
+        examDao.insert(examEntity)
+        val result = examDao.getExam(examId)
+
+        assertEquals(examEntity.id, result.id)
+        // 디비에 들어갔다가 나오면 InstantConverters 에 따라서 값이 바뀌어 있음.
+        assertFalse(examEntity.createdAt == result.createdAt)
     }
 }
