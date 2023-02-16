9ec4e0845cf5109ab8ab95c8afb8623039524070
nosorae <shthfo@gmail.com>
2023-02-16 21:49:33 +0900

    refactor: 문제 테스트케이스 리팩토링

--- data/src/androidTest/java/com/mundcode/data/QuestionDaoTest.kt
+++ data/src/androidTest/java/com/mundcode/data/QuestionDaoTest.kt
@@ -8,9 +8,13 @@ import com.mundcode.data.local.database.MuntamDatabase
 import com.mundcode.data.local.database.dao.ExamDao
 import com.mundcode.data.local.database.dao.QuestionDao
 import com.mundcode.data.local.database.dao.SubjectDao
+import com.mundcode.data.local.database.model.ExamEntity
+import com.mundcode.data.local.database.model.SubjectEntity
 import com.mundcode.data.local.database.model.createExamEntity
+import com.mundcode.data.local.database.model.createQuestionEntities
 import com.mundcode.data.local.database.model.createSubjectEntity
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Assert.assertEquals
@@ -26,9 +30,11 @@ class QuestionDaoTest {
     private lateinit var subjectDao: SubjectDao
     private lateinit var examDao: ExamDao
     private lateinit var db: MuntamDatabase
+    private lateinit var subjectEntity: SubjectEntity
+    private lateinit var examEntity: ExamEntity
 
     @Before
-    fun createDb() {
+    fun createDb() = runBlocking {
         val context = ApplicationProvider.getApplicationContext<Context>()
         db = Room.inMemoryDatabaseBuilder(
             context,
@@ -37,6 +43,14 @@ class QuestionDaoTest {
         questionDao = db.questionDao()
         subjectDao = db.subjectDao()
         examDao = db.examDao()
+
+        val subjectId = SUBJECT_ID
+        val examId = EXAM_ID
+        subjectEntity = createSubjectEntity(subjectId)
+        examEntity = createExamEntity(id = examId, subjectId = subjectId)
+
+        subjectDao.insert(subjectEntity)
+        examDao.insert(examEntity)
     }
 
     @After
@@ -48,17 +62,27 @@ class QuestionDaoTest {
     @Test
     fun testWriteAndReadExam() = runBlocking(Dispatchers.IO) {
         // subjectId 과 examId 모두 0 으로 하면 Foreign Key Constraint Failed  에러
-        val subjectId = 1
-        val examId = 2
-        val subjectEntity = createSubjectEntity(subjectId)
-        val examEntity = createExamEntity(id = examId, subjectId = subjectId)
-
-        subjectDao.insert(subjectEntity)
-        examDao.insert(examEntity)
-        val result = examDao.getExam(examId)
-
+        val result = examDao.getExam(EXAM_ID)
         assertEquals(examEntity.id, result.id)
         // 디비에 들어갔다가 나오면 InstantConverters 에 따라서 값이 바뀌어 있음.
         assertFalse(examEntity.createdAt == result.createdAt)
+    }
+
+    @Test
+    fun testGetQuestions() = runBlocking {
+        val questions = createQuestionEntities(
+            size = 10,
+            subjectId = SUBJECT_ID,
+            examId = EXAM_ID
+        )
+        questionDao.insertAll(questions)
+
+        val result = questionDao.getQuestions(examId = EXAM_ID).firstOrNull()
+        assertEquals(result?.map { it.id }, questions.map { it.id })
+    }
+
+    companion object {
+        const val SUBJECT_ID = 1
+        const val EXAM_ID = 2
     }
 }
