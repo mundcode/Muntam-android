c5278adc591614a5af152bfa8799ab9d605df796
nosorae <shthfo@gmail.com>
2023-02-19 02:20:29 +0900

    chore: 색깔 이름 변경 (to 문탐 전용 색상)

--- app/src/main/java/com/mundcode/muntam/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/mundcode/muntam/presentation/ui/theme/Color.kt
@@ -2,11 +2,12 @@ package com.mundcode.muntam.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Orange = Color(0xFFFF6625)
-val LightOrange = Color(0xFFFFF0E9)
-val Green = Color(0xFF22C71F)
-val LightGreen = Color(0xFFE0FFDF)
-val Red = Color(0xFFFF2969)
+val MTOrange = Color(0xFFFF6625)
+val MTLightOrange = Color(0xFFFFF0E9)
+val MTGreen = Color(0xFF22C71F)
+val MTLightGreen = Color(0xFFE0FFDF)
+val MTRed = Color(0xFFFF2969)
+
 val Gray900 = Color(0xFF212121)
 val Gray800 = Color(0xFF343637)
 val Gray700 = Color(0xFF57595E)
