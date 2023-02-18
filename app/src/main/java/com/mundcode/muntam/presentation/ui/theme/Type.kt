5b76c6c953f7c0baea77607cbbc75bbebeaf7c4f
nosorae <shthfo@gmail.com>
2023-02-19 02:20:00 +0900

    feat: typography 셋업

--- app/src/main/java/com/mundcode/muntam/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/mundcode/muntam/presentation/ui/theme/Type.kt
@@ -1,28 +1,79 @@
 package com.mundcode.muntam.presentation.ui.theme
 
-import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.mundcode.muntam.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+val MTFontFamily = FontFamily(
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_bold, FontWeight.Bold)
 )
+
+object MTTextStyle {
+    val textBold20 = TextStyle(
+        fontSize = 20.sp,
+        fontFamily = MTFontFamily,
+        fontWeight = FontWeight.Bold,
+        lineHeight = 24.sp,
+        letterSpacing = (-0.03).sp
+    )
+
+    val textBold16 = TextStyle(
+        fontSize = 16.sp,
+        fontFamily = MTFontFamily,
+        fontWeight = FontWeight.Bold,
+        lineHeight = 20.sp,
+        letterSpacing = (-0.03).sp
+    )
+
+    val textBold14 = TextStyle(
+        fontSize = 14.sp,
+        fontFamily = MTFontFamily,
+        fontWeight = FontWeight.Bold,
+        lineHeight = 20.sp,
+        letterSpacing = (-0.03).sp
+    )
+
+    val textBold13 = TextStyle(
+        fontSize = 13.sp,
+        fontFamily = MTFontFamily,
+        fontWeight = FontWeight.Bold,
+        lineHeight = 18.sp,
+        letterSpacing = (-0.03).sp
+    )
+
+    val text20 = TextStyle(
+        fontSize = 20.sp,
+        fontFamily = MTFontFamily,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 24.sp,
+        letterSpacing = (-0.03).sp
+    )
+
+    val text16 = TextStyle(
+        fontSize = 16.sp,
+        fontFamily = MTFontFamily,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 20.sp,
+        letterSpacing = (-0.03).sp
+    )
+
+    val text14 = TextStyle(
+        fontSize = 14.sp,
+        fontFamily = MTFontFamily,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 20.sp,
+        letterSpacing = (-0.03).sp
+    )
+
+    val text13 = TextStyle(
+        fontSize = 13.sp,
+        fontFamily = MTFontFamily,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 18.sp,
+        letterSpacing = (-0.03).sp
+    )
+}
