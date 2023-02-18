0524957b10ba6f4e6e2ddc3821ccc35f5510410b
nosorae <shthfo@gmail.com>
2023-02-19 02:03:00 +0900

    feat: 코너 Shape 셋업

--- app/src/main/java/com/mundcode/muntam/presentation/ui/theme/Shape.kt
+++ app/src/main/java/com/mundcode/muntam/presentation/ui/theme/Shape.kt
@@ -4,10 +4,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
-val Shapes = Shapes(
-    small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(8.dp),
-    large = RoundedCornerShape(12.dp)
-)
-
 val Circle = RoundedCornerShape(50)
+val CornerRadius4 = RoundedCornerShape(4.dp)
+val CornerRadius8 = RoundedCornerShape(8.dp)
+val CornerRadius12 = RoundedCornerShape(12.dp)
+val CornerRadius16 = RoundedCornerShape(16.dp)
