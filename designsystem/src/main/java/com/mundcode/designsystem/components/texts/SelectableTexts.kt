44807c96cf04007cd561a5da26acbd1c1c0a7d82
nosorae <shthfo@gmail.com>
2023-02-26 03:24:55 +0900

    feat: SelectableText 가 숫자를 받도록 수정. SelectableNumberText 로 이름 수정

--- designsystem/src/main/java/com/mundcode/designsystem/components/texts/SelectableTexts.kt
+++ designsystem/src/main/java/com/mundcode/designsystem/components/texts/SelectableTexts.kt
@@ -1,11 +1,10 @@
 package com.mundcode.designsystem.components.texts
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
@@ -16,7 +15,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.mundcode.designsystem.theme.Gray900
 import com.mundcode.designsystem.theme.MTOrange
 import com.mundcode.designsystem.theme.MTTextStyle
@@ -24,20 +22,24 @@ import com.mundcode.designsystem.theme.Transparent
 import com.mundcode.designsystem.theme.White
 
 @Composable
-fun SelectableText(
-    text: String,
+fun SelectableNumberText(
+    number: Int,
     isSelected: Boolean,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onClickNumber: (Int) -> Unit = {},
 ) {
     Box(
         modifier = modifier
             .wrapContentSize()
             .size(40.dp) // todo 텍스트 크게 설정한 경우 고려
-            .background(color = if (isSelected) MTOrange else Transparent, shape = CircleShape),
+            .background(color = if (isSelected) MTOrange else Transparent, shape = CircleShape)
+            .clickable {
+                onClickNumber(number)
+            },
         contentAlignment = Alignment.Center
     ) {
         Text(
-            text = text,
+            text = "$number",
             style = if (isSelected) MTTextStyle.textBold16 else MTTextStyle.text16,
             color = if (isSelected) White else Gray900,
             textAlign = TextAlign.Center
@@ -49,13 +51,11 @@ fun SelectableText(
 @Composable
 fun SelectableTextPreview() {
     Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-        SelectableText(text = "1", isSelected = true)
-        SelectableText(text = "1", isSelected = false)
-        SelectableText(text = "2", isSelected = true)
-        SelectableText(text = "2", isSelected = false)
-        SelectableText(text = "3", isSelected = true)
-        SelectableText(text = "3", isSelected = false)
-        SelectableText(text = "4", isSelected = true)
-        SelectableText(text = "5", isSelected = false)
+        SelectableNumberText(number = 1, isSelected = true)
+        SelectableNumberText(number = 2, isSelected = false)
+        SelectableNumberText(number = 3, isSelected = true)
+        SelectableNumberText(number = 4, isSelected = false)
+        SelectableNumberText(number = 100, isSelected = true)
+        SelectableNumberText(number = 100, isSelected = false)
     }
 }
