7655543b7de677b2ad82d74d6c2296d0794c15bf
nosorae <shthfo@gmail.com>
2023-02-26 14:20:42 +0900

    feat: DescriptionDialog 설명 텍스트 가운데 정렬

--- designsystem/src/main/java/com/mundcode/designsystem/components/dialogs/DescriptionDialog.kt
+++ designsystem/src/main/java/com/mundcode/designsystem/components/dialogs/DescriptionDialog.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -53,7 +54,8 @@ fun DescriptionDialog(
             Text(
                 text = description,
                 style = MTTextStyle.text14,
-                color = Gray700
+                color = Gray700,
+                textAlign = TextAlign.Center
             )
 
             Margin(dp = 20.dp)
