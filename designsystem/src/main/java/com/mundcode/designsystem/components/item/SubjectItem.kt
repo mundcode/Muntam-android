d16871179dbcf1596567ef9047cb181707d1d0db
nosorae <shthfo@gmail.com>
2023-02-27 23:54:03 +0900

    feat: 과목 리스트 아이템 파라미터 정의

---
+++ designsystem/src/main/java/com/mundcode/designsystem/components/item/SubjectItem.kt
@@ -0,0 +1,16 @@
+package com.mundcode.designsystem.components.item
+
+import androidx.compose.runtime.Composable
+import kotlinx.datetime.Instant
+
+@Composable
+fun SubjectItem(
+    imoji: String,
+    subjectName: String,
+    lastExamName: String,
+    lastExamDay: Instant,
+    onClickMore: () -> Unit,
+    onClick: () -> Unit,
+) {
+
+}
