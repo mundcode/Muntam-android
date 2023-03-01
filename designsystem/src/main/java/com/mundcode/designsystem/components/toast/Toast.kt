1c4d184e7d0e7b44a56decaad0046551fcb62d84
nosorae <shthfo@gmail.com>
2023-03-01 23:33:24 +0900

    feat: 커스텀 토스트 스크린 추가

--- designsystem/src/main/java/com/mundcode/designsystem/components/toast/Toast.kt
+++ designsystem/src/main/java/com/mundcode/designsystem/components/toast/Toast.kt
@@ -1,5 +1,18 @@
 package com.mundcode.designsystem.components.toast
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mundcode.designsystem.theme.CornerRadius8
+import com.mundcode.designsystem.theme.MTTextStyle
+import com.mundcode.designsystem.theme.MTToastBackground
+import com.mundcode.designsystem.theme.White
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -19,3 +32,27 @@ class ToastState(
     }
 }
 
+
+
+@Composable
+fun MTToast(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = text,
+        style = MTTextStyle.text14,
+        color = White,
+        textAlign = TextAlign.Center,
+        modifier = modifier
+            .background(color = MTToastBackground, shape = CornerRadius8)
+            .fillMaxWidth()
+            .padding(vertical = 12.dp)
+    )
+}
+
+@Preview
+@Composable
+fun MTToastPreview() {
+    MTToast(text = "새로운 과목이 추가되었습니다.", modifier = Modifier.padding(20.dp))
+}
