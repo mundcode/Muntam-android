d3c5d3e2a608d01345f1ebce170ec113e197d5d8
nosorae <shthfo@gmail.com>
2023-03-05 19:50:56 +0900

    chore: MA-44 과목 삭제 컨펌다이얼로그

--- app/src/main/java/com/mundcode/muntam/presentation/screen/main/subjects/SubjectViewModel.kt
+++ app/src/main/java/com/mundcode/muntam/presentation/screen/main/subjects/SubjectViewModel.kt
@@ -1,39 +1,75 @@
 package com.mundcode.muntam.presentation.screen.main.subjects
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.mundcode.domain.model.Subject
 import com.mundcode.domain.usecase.DeleteSubjectUseCase
 import com.mundcode.domain.usecase.GetSubjectsUseCase
+import com.mundcode.muntam.base.BaseViewModel
 import com.mundcode.muntam.presentation.model.SubjectModel
 import com.mundcode.muntam.presentation.model.asStateModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
 class SubjectViewModel @Inject constructor(
     private val getSubjectsUseCase: GetSubjectsUseCase,
     private val deleteSubjectUseCase: DeleteSubjectUseCase,
-) : ViewModel() {
-    private val _subjects = MutableStateFlow<List<SubjectModel>>(listOf())
-    val subjects: StateFlow<List<SubjectModel>> = _subjects.asStateFlow()
-
+) : BaseViewModel<SubjectsState>() {
     init {
         loadSubjects()
     }
 
     private fun loadSubjects() = viewModelScope.launch(Dispatchers.IO) {
         getSubjectsUseCase().collectLatest { list ->
-            _subjects.emit(list.map { it.asStateModel() })
+            updateState {
+                state.value.copy(
+                    subjects = list.map(Subject::asStateModel)
+                )
+            }
         }
     }
 
-    fun deleteSubject(subjectModel: SubjectModel) = viewModelScope.launch(Dispatchers.IO) {
-        deleteSubjectUseCase(subjectModel.id)
+    private suspend fun deleteSubject(subjectModel: SubjectModel) =
+        viewModelScope.launch(Dispatchers.IO) {
+            deleteSubjectUseCase(subjectModel.id)
+        }
+
+    fun onClickDeleteSubject(subjectModel: SubjectModel) {
+        updateState {
+            state.value.copy(
+                showDeleteConfirmDialog = true,
+                selectedModel = subjectModel
+            )
+        }
+    }
+
+    fun onClickDeleteSubjectConfirm() = viewModelScope.launch {
+        state.value.selectedModel?.let {
+            deleteSubject(it)
+            updateState {
+                state.value.copy(
+                    showDeleteConfirmDialog = false
+                )
+            }
+        }
+    }
+
+    fun onCancelDialog() = updateState {
+        state.value.copy(
+            showDeleteConfirmDialog = false
+        )
+    }
+
+    override fun createInitialState(): SubjectsState {
+        return SubjectsState()
     }
 }
+
+data class SubjectsState(
+    val subjects: List<SubjectModel> = listOf(),
+    val selectedModel: SubjectModel? = null,
+    val showDeleteConfirmDialog: Boolean = false
+)
