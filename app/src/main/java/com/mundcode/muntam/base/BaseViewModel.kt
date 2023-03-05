fbb64205b42f7a9e3feb60fb0b7798530005ebe9
nosorae <shthfo@gmail.com>
2023-03-05 19:20:54 +0900

    feat: MA-44 BaseViewModel 향상

--- app/src/main/java/com/mundcode/muntam/base/BaseViewModel.kt
+++ app/src/main/java/com/mundcode/muntam/base/BaseViewModel.kt
@@ -1,5 +1,26 @@
 package com.mundcode.muntam.base
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
-open class BaseViewModel : ViewModel()
+abstract class BaseViewModel<T> : ViewModel() {
+
+    protected val _state = MutableStateFlow(createInitialState())
+    val state: StateFlow<T> = _state
+
+    protected val mutex = Mutex()
+
+
+    abstract fun createInitialState(): T
+
+    open fun updateState(newState: () -> T) = viewModelScope.launch {
+        mutex.withLock {
+            _state.value = newState()
+        }
+    }
+}
