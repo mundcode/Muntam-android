7e4f63edfdf6ad97b29ca330d3338841a98b1acd
nosorae <shthfo@gmail.com>
2023-03-05 23:36:43 +0900

    feat: MA-48 시험리스트 화면 뼈대 작성

--- app/src/main/java/com/mundcode/muntam/presentation/screen/exams/ExamsScreen.kt
+++ app/src/main/java/com/mundcode/muntam/presentation/screen/exams/ExamsScreen.kt
@@ -1,94 +1,59 @@
 package com.mundcode.muntam.presentation.screen.exams
 
-import android.Manifest
-import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material.Divider
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.isGranted
-import com.google.accompanist.permissions.rememberPermissionState
-import com.google.accompanist.permissions.shouldShowRationale
+import androidx.compose.ui.Modifier
+import com.mundcode.designsystem.components.toolbars.MTTitleToolbar
+import com.mundcode.designsystem.theme.Gray200
 import com.mundcode.muntam.util.hiltViewModel
 
 @Composable
 fun ExamsScreen(
-    subjectId: Int,
     viewModel: ExamsViewModel = hiltViewModel(),
-    onNavEvent: (String) -> Unit
+    onNavEvent: (String) -> Unit,
+    onClickBack: () -> Unit
 ) {
-    val exams = viewModel.exams.collectAsState(listOf())
-
-    LaunchedEffect(key1 = true) {
-        viewModel.getExams(subjectId)
-    }
-
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(4.dp)
-    ) {
-        item {
-            Row {
-                Button(onClick = {
-                    viewModel.insertExam(subjectId)
-                }) {
-                    Text(text = "insert")
-                }
-
-                Button(onClick = {
-                    viewModel.updateExam(exams.value.random())
-                }) {
-                    Text(text = "update")
-                }
-
-                Button(onClick = {
-                    viewModel.deleteExam(exams.value.random())
-                }) {
-                    Text(text = "delete")
+    val state by viewModel.state.collectAsState()
+    Scaffold(
+        topBar = {
+            MTTitleToolbar(
+                title = state.subjectTitle,
+                onClickBack = onClickBack
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+        ) {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                items(state.exams) { item ->
+                    Column {
+                        ExamItem(exam = item)
+                        Divider(Modifier.fillMaxWidth(), color = Gray200)
+                    }
                 }
             }
-        }
 
-        items(exams.value) { model ->
-            Text(text = model.name, fontSize = 42.sp)
-        }
-    }
-}
-
-@OptIn(ExperimentalPermissionsApi::class)
-@Composable
-fun TestScreen() {
-    val notificationPermissionState =
-        rememberPermissionState(permission = Manifest.permission.POST_NOTIFICATIONS) { result ->
-            Log.d("SR-N", "result $result")
-        }
-
-    val text by derivedStateOf {
-        when {
-            notificationPermissionState.status.isGranted -> "이미 권한 허용됨"
-            notificationPermissionState.status.shouldShowRationale -> "Show rationale"
-            else -> "권한 요청"
+            // todo 시간 기록하기 버튼 추가
+            // todo 광고 뷰 추가
         }
     }
 
-    Button(onClick = {
-        if (!notificationPermissionState.status.isGranted) {
-            if (notificationPermissionState.status.shouldShowRationale) {
-                Log.d("SR-N", "Show rationale")
-            } else {
-                notificationPermissionState.launchPermissionRequest()
-            }
-        }
-    }) {
-        Text(text = text)
-    }
+    // todo 다이얼로그들 추가
+    // todo 토스트 추가
+
 }
