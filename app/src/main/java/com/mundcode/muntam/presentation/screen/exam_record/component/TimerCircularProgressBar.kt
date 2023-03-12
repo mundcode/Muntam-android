5b19bd21845d828b3d2424cdd9629276ac538d25
nosorae <shthfo@gmail.com>
2023-03-12 19:34:16 +0900

    feat: MA-49 타이머 원모양 상태바 커스텀 뷰 추가

---
+++ app/src/main/java/com/mundcode/muntam/presentation/screen/exam_record/component/TimerCircularProgressBar.kt
@@ -0,0 +1,125 @@
+package com.mundcode.muntam.presentation.screen.exam_record.component
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mundcode.designsystem.theme.Gray100
+import com.mundcode.designsystem.theme.Gray500
+import com.mundcode.designsystem.theme.Gray900
+import com.mundcode.designsystem.theme.MTOrange
+import com.mundcode.designsystem.theme.MTRed
+import com.mundcode.designsystem.theme.MTTextStyle
+import com.mundcode.designsystem.util.spToDp
+import com.mundcode.muntam.presentation.screen.exam_record.LINE_WIDTH
+import com.mundcode.muntam.presentation.screen.exam_record.PROGRESS_ANIM_DURATION_MILLIES
+
+@Composable
+fun TimerCircularProgressBar(
+    modifier: Modifier = Modifier,
+    backgroundLineColor: Color,
+    lineColor: Color,
+    prevPercentage: Float = 0f,
+    newPercentage: Float,
+    currentTime: String,
+    remainTime: String
+) {
+    val currentPercentage = remember { Animatable(prevPercentage) }
+
+    LaunchedEffect(newPercentage) {
+        currentPercentage.animateTo(
+            newPercentage, animationSpec = tween(durationMillis = PROGRESS_ANIM_DURATION_MILLIES)
+        )
+    }
+
+
+    Box(
+        modifier = modifier.padding((LINE_WIDTH / 2).dp), contentAlignment = Alignment.Center
+    ) {
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            drawArc(
+                color = backgroundLineColor,
+                startAngle = 0f,
+                sweepAngle = 360f,
+                useCenter = false,
+                style = Stroke(width = LINE_WIDTH.dp.toPx())
+            )
+
+            drawArc(
+                color = lineColor,
+                startAngle = -90f,
+                sweepAngle = 360f * currentPercentage.value,
+                useCenter = false,
+                style = Stroke(width = LINE_WIDTH.dp.toPx(), cap = StrokeCap.Round)
+            )
+        }
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TimeText(description = "현재시간", timeText = currentTime, color = Gray900)
+            TimeText(description = "남은시간", timeText = remainTime, color = MTRed)
+        }
+    }
+}
+
+@Composable
+fun TimeText(
+    modifier: Modifier = Modifier,
+    description: String,
+    timeText: String,
+    color: Color
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = description, style = MTTextStyle.text13.spToDp(), color = Gray500)
+        Text(text = timeText, style = MTTextStyle.timeTextStyle.spToDp(), color = color)
+    }
+}
+
+@Preview(widthDp = 300, heightDp = 300)
+@Composable
+fun TimerCircularProgressBarPreview() {
+    TimerCircularProgressBar(
+        backgroundLineColor = Gray100,
+        lineColor = MTOrange,
+        newPercentage = 0.8f,
+        modifier = Modifier.size(296.dp),
+        currentTime = "02:23:11",
+        remainTime = "00:37:49"
+    )
+}
+
+@Preview(widthDp = 300, heightDp = 300)
+@Composable
+fun TimerCircularProgressBarPreview2() {
+    TimerCircularProgressBar(
+        backgroundLineColor = Gray100,
+        lineColor = MTOrange,
+        prevPercentage = 0.5f,
+        newPercentage = 0.8f,
+        modifier = Modifier.size(296.dp),
+        currentTime = "02:23:11",
+        remainTime = "00:37:49"
+    )
+}
